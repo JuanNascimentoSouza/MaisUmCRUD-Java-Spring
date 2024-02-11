9a52f15f068342bfe0eb1c7ff24b9195877d703a
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2024-02-10 23:20:33 -0300

    att product att controller

---
+++ CRUD/src/main/java/com/example/CRUD/domain/product/Product.java
@@ -0,0 +1,33 @@
+package com.example.CRUD.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Number price_in_cents;
+    private Boolean active;
+
+    public Product(RequestProduct requestProduct) {
+        this.name = requestProduct.name();
+        this.price_in_cents = requestProduct.price_in_cents();
+        this.active = true;
+    }
+
+    public static void setName(String name) {
+    }
+
+    public static void setPrice_in_cents(Number price_in_cents) {
+
+    }
+}
