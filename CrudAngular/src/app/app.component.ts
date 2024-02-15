8739304b856f01af2e5a3dff10f2fc1649f25c08
JuanNascimentoSouza <juan.nascimento.999@gmail.com>
2024-02-15 18:51:53 -0300

    mattabledatasource, mattablemodule, compotent html and module

--- CrudAngular/src/app/app.component.ts
+++ CrudAngular/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
