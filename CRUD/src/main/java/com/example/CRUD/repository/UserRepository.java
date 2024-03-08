e073e023d967a46d2e45eb67a043ecfaa6141d97
JuanNascimentoSouza <juan.nascimento.999@gmail.com>
2024-03-07 22:47:43 -0300

    UserService, att UserRepository

--- CRUD/src/main/java/com/example/CRUD/repository/UserRepository.java
+++ CRUD/src/main/java/com/example/CRUD/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.example.CRUD.repository;
 import com.example.CRUD.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     User save(User user);
+
+    Optional<User> findById(Long id);
+
+    void deleteById(Long id);
 }
---
+++ CRUD/src/main/java/com/example/CRUD/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.CRUD.service;
+
+import com.example.CRUD.domain.User;
+import com.example.CRUD.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
