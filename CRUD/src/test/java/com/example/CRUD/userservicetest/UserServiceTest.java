25928b43dbf59a750c13b4accf077307bce81b9d
JuanNascimentoSouza <juan.nascimento.999@gmail.com>
2024-03-11 23:56:34 -0300

    Tests

---
+++ CRUD/src/test/java/com/example/CRUD/userservicetest/UserServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.CRUD.userservicetest;
+
+import com.example.CRUD.domain.User;
+import com.example.CRUD.repository.UserRepository;
+import com.example.CRUD.service.UserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static javax.management.Query.times;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User(1L,"Test","test@example.com");
+
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User createdUser = userService.createUser(user);
+
+        assertEquals(user.getName(),createdUser.getName());
+
+        assertEquals(user.getEmail(),createdUser.getEmail());
+
+    }
+
+    @Test
+    public void testGetUserById() {
+        User user = new User(1L,"Test2","test@example.com");
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        User foundUser = userService.getUserById(1L);
+
+        assertEquals(user.getName(),foundUser.getName());
+
+        assertEquals(user.getEmail(),foundUser.getEmail());
+    }
+
+
+
+}
