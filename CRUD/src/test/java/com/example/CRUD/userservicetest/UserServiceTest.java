8b1719ba035f6d0e6b66d0878d6c0460d9ac44f6
JuanNascimentoSouza <juan.nascimento.999@gmail.com>
2024-03-12 17:56:10 -0300

    Test verify bugfix

--- CRUD/src/test/java/com/example/CRUD/userservicetest/UserServiceTest.java
+++ CRUD/src/test/java/com/example/CRUD/userservicetest/UserServiceTest.java
@@ -10,8 +10,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
-import static javax.management.Query.times;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -28,30 +28,33 @@ public class UserServiceTest {
     @Test
     public void testCreateUser(){
         User user = new User(1L,"Test","test@example.com");
-
         when(userRepository.save(any(User.class))).thenReturn(user);
 
         User createdUser = userService.createUser(user);
 
         assertEquals(user.getName(),createdUser.getName());
-
         assertEquals(user.getEmail(),createdUser.getEmail());
-
+        verify(userRepository).save(user);
     }
 
     @Test
     public void testGetUserById() {
         User user = new User(1L,"Test2","test@example.com");
-
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
         User foundUser = userService.getUserById(1L);
 
         assertEquals(user.getName(),foundUser.getName());
-
         assertEquals(user.getEmail(),foundUser.getEmail());
     }
 
+    @Test
+    public void testGetById_UserNotFound(){
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
+        User foundUser = userService.getUserById(1L);
+
+        assertNull(foundUser,"User should be null");
+    }
 
 }
