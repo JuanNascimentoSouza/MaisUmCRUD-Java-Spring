c411acf6296d662e8733b0ee95deafd9a8af7c7a
JuanNascimentoSouza <juan.nascimento.999@gmail.com>
2024-04-12 22:11:55 -0300

    front

--- CRUD/src/main/java/com/example/CRUD/domain/product/RequestProduct.java
+++
@@ -1,7 +0,0 @@
-package com.example.CRUD.domain.product;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record RequestProduct(String id, @NotBlank String name, @NotNull Integer price_in_cents) {
-}
